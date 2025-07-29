61eeb4998297b8bfc3e200ff983a617630e97555
Staniel Hristov <stanielhhristov@gmail.com>
2025-07-29 11:28:07 +0300

    Initial commit

---
+++ src/test/java/com/reservation/medical_reservation/MedicalReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.medical_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
